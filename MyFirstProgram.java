ab1e03dc1b0f99724e9f0f8519e0e59505d25869
Miral Patel <patelmv.2995@gmail.com>
2022-07-19 20:55:47 +0100

    MyFirstProgram

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+package JavaConcepts;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My Frist Application");
+        System.out.print("MY Second Line");
+        System.out.println("Tester");
+    }
+}
