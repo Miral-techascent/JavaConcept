f6acb25908ad7723c239cf3a956a50b915c9c92e
Miral Patel <patelmv.2995@gmail.com>
2022-07-19 22:17:14 +0100

    MyFirstProgram update

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -4,7 +4,7 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("My Frist Application");
         System.out.print("MY Second Line");
-        System.out.println("Tester");
+        System.out.println("Tester is here");
         System.out.println("Hello, How are you?");
     }
 }
